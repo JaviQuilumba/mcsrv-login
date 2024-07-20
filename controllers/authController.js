78b75a3dbbb77dc398c0565c3cc47acd2b1eee17
JaviQuilumba <javier0611quilumba@gmail.com>
2024-07-20 14:32:55 -0500

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+// authController.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/loginModel'); 
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await User.findByEmail(email);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const user = result.rows[0];
+
+    if (password !== user.password) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, 'servicelogintoken', { expiresIn: '1h' });
+
+    res.status(200).json({ token, role: user.role });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const healthCheck = (req, res) => {
+  res.status(200).send('OK');
+};
+
+module.exports = {
+  login,
+  healthCheck,
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerConfig');
+const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Microservice Login Running' });
+});
+
+app.use(cors({
+  origin: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com'
+}));
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
